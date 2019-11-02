459bf3b04cb9dd34add93c7a107044f30ae58e68
biabs1 <beatriz.souza@ccc.ufcg.edu.br>
2019-11-02 13:37:56 -0300

    initial commit

---
+++ src/main/java/psoftProjectBack/psoftProjectBack/PsoftProjectBackApplication.java
@@ -0,0 +1,13 @@
+package psoftProjectBack.psoftProjectBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsoftProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsoftProjectBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psoftProjectBack/psoftProjectBack/PsoftProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package psoftProjectBack.psoftProjectBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsoftProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
