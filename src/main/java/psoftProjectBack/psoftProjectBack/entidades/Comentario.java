7289c18ade1f2df450155626783a13799ee3fe6b
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2019-11-09 15:37:25 -0300

    alteracao comentario

--- src/main/java/psoftProjectBack/psoftProjectBack/entidades/Comentario.java
+++ src/main/java/psoftProjectBack/psoftProjectBack/entidades/Comentario.java
@@ -8,11 +8,6 @@ import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
 public class Comentario {
 
