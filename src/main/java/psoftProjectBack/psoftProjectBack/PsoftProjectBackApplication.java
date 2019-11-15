db24e659c56d9a2a5e9f237c96e6bd3ab7248b44
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2019-11-15 13:10:34 -0300

    add de filtro para verificar tokens

--- src/main/java/psoftProjectBack/psoftProjectBack/PsoftProjectBackApplication.java
+++ src/main/java/psoftProjectBack/psoftProjectBack/PsoftProjectBackApplication.java
@@ -2,9 +2,24 @@ package psoftProjectBack.psoftProjectBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import psoftProjectBack.psoftProjectBack.filtros.FiltroToken;
 
 @SpringBootApplication
 public class PsoftProjectBackApplication {
+
+	@Bean
+	public FilterRegistrationBean<FiltroToken> filtroJwt() {
+
+		FilterRegistrationBean<FiltroToken> filterRB = new FilterRegistrationBean<FiltroToken>();
+
+		filterRB.setFilter(new FiltroToken());
+		filterRB.addUrlPatterns("/campanha/*");
+		return filterRB;
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PsoftProjectBackApplication.class, args);
