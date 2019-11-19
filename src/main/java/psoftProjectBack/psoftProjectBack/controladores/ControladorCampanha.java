f42b29f6f7ca25c72e323555487cfc1a07a0eb71
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2019-11-19 00:12:18 -0300

    add finalizacoes

--- src/main/java/psoftProjectBack/psoftProjectBack/controladores/ControladorCampanha.java
+++ src/main/java/psoftProjectBack/psoftProjectBack/controladores/ControladorCampanha.java
@@ -1,13 +1,11 @@
 package psoftProjectBack.psoftProjectBack.controladores;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.servlet.ServletException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
--- src/main/java/psoftProjectBack/psoftProjectBack/controladores/ControladorUsuario.java
+++ src/main/java/psoftProjectBack/psoftProjectBack/controladores/ControladorUsuario.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
