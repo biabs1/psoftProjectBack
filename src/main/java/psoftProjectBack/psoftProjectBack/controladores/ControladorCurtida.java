77a5ef2596cb30e243f6564029cfb93523625355
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2019-11-28 10:17:46 -0300

    modificacoes na curtida

--- src/main/java/psoftProjectBack/psoftProjectBack/controladores/ControladorCurtida.java
+++ src/main/java/psoftProjectBack/psoftProjectBack/controladores/ControladorCurtida.java
@@ -1,7 +1,11 @@
 package psoftProjectBack.psoftProjectBack.controladores;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,48 +21,54 @@ import psoftProjectBack.psoftProjectBack.servicos.ServicoCurtida;
 import psoftProjectBack.psoftProjectBack.servicos.ServicoJWT;
 import psoftProjectBack.psoftProjectBack.servicos.ServicoUsuario;
 
-@Api(value="Sistema de gerenciamento de curtida", description="Controlador do gerenciamento de curtidas das campanhas")
+@Api(value = "Sistema de gerenciamento de curtida", description = "Controlador do gerenciamento de curtidas das campanhas")
 @RestController
 public class ControladorCurtida {
-	
+
 	private ServicoCurtida servicoCurtida;
 	private ServicoJWT servicoJWT;
 	private ServicoUsuario servicoUsuario;
 	private ServicoCampanha servicoCampanha;
-	
-	public ControladorCurtida(ServicoCurtida sCurtida, ServicoJWT sJWT,
-			ServicoUsuario sUsuario, ServicoCampanha sCampanha) {
+
+	public ControladorCurtida(ServicoCurtida sCurtida, ServicoJWT sJWT, ServicoUsuario sUsuario,
+			ServicoCampanha sCampanha) {
 		super();
 		servicoCurtida = sCurtida;
 		servicoJWT = sJWT;
 		servicoUsuario = sUsuario;
 		servicoCampanha = sCampanha;
 	}
-	
+
 	@PostMapping("/campanha/{id}/curtir")
-	public ResponseEntity<Curtida> addRemoveCurtida(@RequestBody Curtida curtida,
-			@PathVariable("id") long id, 
+	public ResponseEntity<Curtida> addRemoveCurtida(@RequestBody Curtida curtida, @PathVariable("id") long id,
 			@RequestHeader("Authorization") String header) throws Exception {
-		
+
 		String email = servicoJWT.recuperarSujeitoDoToken(header);
 		if (!servicoUsuario.getUsuario(email).isPresent()) {
 			return new ResponseEntity<Curtida>(HttpStatus.NOT_FOUND);
 		}
-		
+
 		Campanha campanha = servicoCampanha.acessaCampanha(id).get();
 		Usuario usuario = servicoUsuario.getUsuario(email).get();
-		
+
 		if (servicoCampanha.jaCurtiu(campanha, usuario)) {
-			//servicoCampanha.removerCurtida(campanha, curtida);
-			servicoCurtida.removerCurtida(curtida);
-			return new ResponseEntity<Curtida>(curtida, HttpStatus.OK);
-			//throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Você já curtiu esta campanha");
+			return new ResponseEntity<Curtida>(this.servicoCurtida.removerCurtida(curtida), HttpStatus.OK);
 		}
-		
+
 		curtida.setCampanha(campanha);
 		curtida.setQuemDeuLike(usuario);
-		
+
 		return new ResponseEntity<Curtida>(servicoCurtida.adicionarCurtida(curtida), HttpStatus.CREATED);
+
+	}
+
+	@GetMapping("/campanha/{id}/curtir")
+	public ResponseEntity<List<Curtida>> quaisCurtiram(@PathVariable("id") long id,
+			@RequestHeader("Authorization") String header) throws Exception {
+
+		Optional<Campanha> campanha = this.servicoCampanha.acessaCampanha(id);
+
+		return new ResponseEntity<List<Curtida>>(servicoCurtida.quaisCurtiram(campanha.get()), HttpStatus.OK);
 
 	}
 
--- src/main/java/psoftProjectBack/psoftProjectBack/servicos/ServicoCurtida.java
+++ src/main/java/psoftProjectBack/psoftProjectBack/servicos/ServicoCurtida.java
@@ -1,33 +1,49 @@
 package psoftProjectBack.psoftProjectBack.servicos;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import psoftProjectBack.psoftProjectBack.entidades.Campanha;
 import psoftProjectBack.psoftProjectBack.entidades.Curtida;
 import psoftProjectBack.psoftProjectBack.repositorios.RepositorioCurtida;
 
 @Service
 public class ServicoCurtida {
-	
+
 	private RepositorioCurtida<Curtida, Long> curtidasDAO;
-	
+
 	public ServicoCurtida(RepositorioCurtida<Curtida, Long> curtidasDAO) {
 		super();
 		this.curtidasDAO = curtidasDAO;
 	}
-	
+
 	public Curtida adicionarCurtida(Curtida curtida) {
 		return this.curtidasDAO.save(curtida);
 	}
-	
+
 	public Optional<Curtida> getCurtida(Long id) {
 		return this.curtidasDAO.findById(id);
 	}
 
-	public void removerCurtida(Curtida curtida) {
+	public Curtida removerCurtida(Curtida curtida) {
 		this.curtidasDAO.delete(curtida);
+		return this.curtidasDAO.save(curtida);
 	}
-	
+
+	public List<Curtida> quaisCurtiram(Campanha campanha) {
+
+		List<Curtida> curtidasDaCampanha = new ArrayList<>();
+
+		for (Curtida c : this.curtidasDAO.findAll()) {
+			if (c.getCampanha().getId() == campanha.getId()) {
+				curtidasDaCampanha.add(c);
+			}
+		}
+
+		return curtidasDaCampanha;
+	}
 
 }
